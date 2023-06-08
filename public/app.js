3a5b0a1618e851b4cc341896217c05f3d18f3ec1
simplypstar <97260718+simplypstar@users.noreply.github.com>
2023-06-07 22:19:19 -0400

    Final commit

--- public/app.js
+++ public/app.js
@@ -24,7 +24,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 const marker = L.marker([48.87007, 2.346453])
 marker.addTo(myMap).bindPopup('<p1><b>The Hoxton, Paris</b></p1>').openPopup()
 
-// Draw the 2nd arrondissement
 // Draw the 2nd Arrondissement                                          
 const latlngs = [[48.863368120198004, 2.3509079846928516],[48.86933262048345, 2.3542531602919805],[48.87199261164275, 2.3400569901592183],[48.86993336274516, 2.3280142476578813], [48.86834104280146, 2.330308418109664]]
 
